7effd4fe81752db064df1072918da432ea67740d
Francis Wambugu <95134949+Nevyseal@users.noreply.github.com>
2025-04-29 11:22:44 +0300

    Create CarPartDetail.java

---
+++ CarPartDetail.java
@@ -0,0 +1,11 @@
+package app.ij.mlwithtensorflowlite;
+
+public class CarPartDetail {
+    public String name;
+    public String make;
+    public String year;
+    public String source;
+    public String position;
+    public int difficulty;
+    public String description;
+}
